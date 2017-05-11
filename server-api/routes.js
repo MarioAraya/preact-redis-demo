5643c208171b012d80630ebba070da7f2324eddc
Mario Araya Romero <arayaromero@gmail.com>
2017-05-11 18:23:36 -0300

    API métodos para ler y guardar en redis. y leer de google maps API

--- server-api/routes.js
+++ server-api/routes.js
@@ -1,9 +1,10 @@
 let express = require('express')
-let router = express();    
+let router = express();
 let redis = require('redis')
 let axios = require('axios')
 let request = require('request')
 
+const url = "http://localhost:5000";
 
 module.exports = function(app) {
     // Redis connect
@@ -15,55 +16,62 @@ module.exports = function(app) {
         console.log('error: ' + err)
     })
 
-
-    app.get('/api/redis/getall', function(req, res, next) {
-        redisClient.hgetall("Santiago,CL", function(err, obj) {
-            if( !obj ) {
-                console.log('No cities saved.')
+    app.get('/api/redis/get-city/:ciudad', function(req, res, next) {
+        redisClient.hgetall(req.params.ciudad, function(err, obj) {
+            if (!obj) {
+                console.log('Ciudad no registrada en redis. Se procede a sacar data de GoogleMaps y guardarla en Redis.')
+                request.get(url + "/api/redis/store/" + req.params.ciudad, (error, response, body) => {
+                    if (error) { return res.sendStatus(500); }
+                    return saveLatLngEnRedis(req.params.ciudad, body);
+                });
             } else {
-                console.log(obj)
+                return getLatLngRedis(req.params.ciudad);
             }
             res.send(obj);
         });
     })
 
-    app.get('/api/redis/store', function(req, res) {
-        //Setear lat/long en dos SET de redis, no tratar de guardar json
-        redisClient.hmset("Santiago,CL", [
-            'lattitude', -33.459229,
-            'longitude', -70.645348
-        ], function( err, reply) {
-            if(err) console.log(err);
-            console.log(reply);
-        })
-        redisClient.hmset("Zurich,SW", [
-            'lattitude', -7.451542,
-            'longitude', -8.564572
-        ], function( err, reply) {
-            if(err) console.log(err);
-            console.log(reply);
-        })
+    app.get('/api/redis/store/:ciudad', function(req, res) {
+        request.get(url + "/api/google-api/" + req.params.ciudad, (error, response, body) => {
+            if (error) {
+                return res.sendStatus(500);
+            }
+            return saveLatLngEnRedis(req.params.ciudad, body);
+        });
     })
 
     // Obtiene coordenadas desde API google :ciudad string. ej:"Santiago,CL"
     app.get('/api/google-api/:ciudad', function(req, res) {
         request.get({
-            url: 'https://maps.google.com/maps/api/geocode/json?key=AIzaSyBsQYXKHEPSZ7iiapj7okgxoar6KPsypHM&address=' + req.params.ciudad,
+            url: 'https://maps.google.com/maps/api/geocode/json?key=AIzaSyAIDZiwD2-lgitBaK_HVsFZMMRjxCsKEug&address=' + req.params.ciudad,
             json: true
-        }, function(error, response, body){
-            if(error){
-                console.log('ERROR with user request.');
+        }, function(error, response, body) {
+            if (error) {
+                console.log('Error al tratar de consumir API google. ' + error)
                 return res.sendStatus(500);
-            }
-            else {
-                return res.status(200).send(body.results[0].geometry.location);       
+            } else {
+                return res.status(200).send(body.results[0].geometry.location);
             }
         });
     })
 
-    app.get('/', function (req, res) {
-        res.send('GET request to the homepage CTM')
+    // Raiz
+    app.get('/', function(req, res) {
+        res.send('API root /')
     })
+
+    // Guardar en redis: HMSET Ciudad,[lat,lng]
+    saveLatLngEnRedis = function(keyCiudad, jsonLatLng) {
+        redisClient.hmset(keyCiudad, [
+            'lat', JSON.parse(jsonLatLng).lat,
+            'lng', JSON.parse(jsonLatLng).lng
+        ], function(err, reply) {
+            if (err) console.log('Error al tratar de guardar en Redis. ' + err)
+            return reply;
+        })
+    }
+
+    getLatLngRedis = function(keyCiudad) {
+        return redisClient.hgetall(keyCiudad);
+    }
 }
-
-
