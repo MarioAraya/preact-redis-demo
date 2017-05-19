ad015c2517f627e0d2d06ba1620596fd8013d21c
Mario Araya Romero <arayaromero@gmail.com>
2017-05-19 11:33:24 -0400

    se mueven archivos /app-server arriba, a folder raiz... esto para simplificar subida a heroku

--- server-api/server.js
+++
@@ -1,7 +0,0 @@
-let express = require('express')
-let app = express()
-let routes = require('./routes')(app)
-
-app.listen(5000, function() {
-    console.log('Servidor escuchando en puerto 5000')
-})
