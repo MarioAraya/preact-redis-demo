d4a1ed63c82ec4161fb2a7b0e62f0ae8f56042e2
Mario Araya Romero <arayaromero@gmail.com>
2017-05-19 12:48:46 -0400

    process.env.PORT added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let express = require('express')
 let app = express()
 let routes = require('./routes')(app)
 
-app.listen(5000, function() {
-    console.log('Servidor escuchando en puerto 5000')
-})
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Servidor escuchando en puerto ', app.get('port'));
+});
