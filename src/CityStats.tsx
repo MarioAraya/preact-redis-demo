b688529bc1196d7cee7ff3cfb0e8b3cb7fdb59a3
Mario Araya Romero <arayaromero@gmail.com>
2017-05-20 16:55:40 -0400

    url = heroku-created-app-url

--- src/CityStats.tsx
+++ src/CityStats.tsx
@@ -2,7 +2,8 @@ import { h, Component } from "preact";
 import axios from "axios";
 import utils from "./util"
 
-const url: string = "https://localhost:5000";
+//const url: string = "https://localhost:5000";
+const url: string = "https://cryptic-retreat-74751.herokuapp.com/";
 
 export interface ICityProps {
     nombre?: string;
