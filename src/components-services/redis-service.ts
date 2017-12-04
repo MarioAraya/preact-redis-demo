d55a752815cf6361e9cccfacb2c41fc8af66ad4a
Mario Araya Romero <arayaromero@gmail.com>
2017-12-04 10:26:36 -0300

    added empty param validation

--- src/components-services/redis-service.ts
+++ src/components-services/redis-service.ts
@@ -4,12 +4,17 @@ import * as constants from './constants'
 export default {
     // lat Lng from redis
     getDataCiudad: function(ciudad: string): Promise<any> {
-        return constants.httpClient.get("/api/redis/getLatLng/" + ciudad).then( res => {
-            console.log(`getStats OK nombre=${ciudad} lat=${res.data.lat} lng=${res.data.lng}`)
-            return forecastService.getForecast(res.data)
-        }).catch(err => {
-            console.log('Error en /api/redis/getLatLng/ : ' +err)
-            return err
-        })
+        return constants.httpClient.get("/api/redis/getLatLng/" + ciudad)
+            .then( res => {
+                if (!res) {
+                    console.log('No se encontró datos para ' + ciudad)
+                    return
+                }
+                console.log(`getStats OK nombre=${ciudad} lat=${res.data.lat} lng=${res.data.lng}`)
+                return forecastService.getForecast(res.data)
+            }).catch(err => {
+                console.log('Error en /api/redis/getLatLng/ : ' +err)
+                return err
+            })
     }
 }
