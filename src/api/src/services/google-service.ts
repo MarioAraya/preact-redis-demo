9ca2c5fa4bb7250158e21e218e56d14f2ee0029d
Mario Araya <mario.araya@globant.com>
2018-08-28 14:57:20 -0300

    UI info details + css effect on click + removed unused method

--- src/api/src/services/google-service.ts
+++ src/api/src/services/google-service.ts
@@ -1,26 +1,12 @@
-import axios from 'axios'
-import * as etcService from './etc-service'
 import * as constants from './constants'
 
 export default {
-    // googleGetLatLng(ciudad: string, res: any) {
-    //     return request.get(this.etcService.googleApiUrl() + ciudad, (error, response, body) => {
-    //         if (error) {
-    //             console.log('Error en /api/googlemaps/getLatLng/ : ' +error)
-    //             return res.sendStatus(500);
-    //         }
-    //         //console.log('/api/googlemaps/getLatLng/' +req.params.ciudad +' : ' +JSON.parse(body).results[0].geometry.location)
-    //         let locationObjParsed = JSON.parse(body).results[0].geometry.location;
-    //         return res.send(locationObjParsed) 
-    //     })
-    // },
-
     getCoordenadas(ciudad: string): Promise<any> {
         if(!ciudad) return
         
         return constants.httpClient.get(constants.googleApiUrl + ciudad)
             .then( resForecast => {
-                console.log('__response '+ ciudad +' ', resForecast.status)
+                console.log('__response '+ ciudad +' location = ', resForecast.data.results[0].geometry.location)
                 return resForecast.data.results[0].geometry.location;
             })
             .catch( err => {
--- src/app.tsx
+++ src/app.tsx
@@ -9,5 +9,8 @@ render(
         <CityStats nombreCiudad="Sydney,AU" />
         <CityStats nombreCiudad="London,UK" />
         <CityStats nombreCiudad="Malloco,CL" />
+        <p><i>Note: Click loading icon to refresh forecast information.</i></p>
+        <p><i>this is using googlemaps to get lat/lng, then using forecast.io to query data, caching latlng in Redis instance </i></p>
+        <a href="https://github.com/MarioAraya/preact-redis-demo">https://github.com/MarioAraya/preact-redis-demo</a>
     </div>
 , document.querySelector("#app"));
