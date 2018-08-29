fd2faa100463c419f8110936cc2810feefa7eb0e
Mario Araya <mario.araya@globant.com>
2018-08-28 22:58:12 -0300

    trying still to deploy ...

--- server/api/src/index.ts
+++ server/api/src/index.ts
@@ -5,10 +5,10 @@ import * as cors from 'cors'
 
 let app = express()
 
-app.set('port', constants.appApiPort)
+// app.set('port', process.env.PORT)
 
 routes.SetRoutes(app)
 
-app.listen(app.get('port'), function() {
-  console.log('Servidor escuchando en puerto ', app.get('port'))
+app.listen(process.env.PORT, function() {
+  console.log('Servidor escuchando en puerto ', process.env.PORT)
 })
