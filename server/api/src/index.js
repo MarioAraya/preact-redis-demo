d93ae02862c37ab1ef0c7973a0d6da1ab16c69fe
Mario Araya <mario.araya@globant.com>
2018-08-28 23:28:03 -0300

    minor fix

--- server/api/src/index.js
+++ server/api/src/index.js
@@ -2,10 +2,9 @@
 exports.__esModule = true;
 var express = require("express");
 var routes_1 = require("./routes");
-var constants = require("./services/constants");
 var app = express();
-app.set('port', constants.appApiPort);
+// app.set('port', process.env.PORT)
 routes_1["default"].SetRoutes(app);
-app.listen(app.get('port'), function () {
-    console.log('Servidor escuchando en puerto ', app.get('port'));
+app.listen(process.env.PORT, function () {
+    console.log('Servidor escuchando en puerto ', process.env.PORT);
 });
